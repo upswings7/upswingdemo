b2e2891f7b7b801cddf9505f0a63c93c7177ee41
upswings7 <68121569+upswings7@users.noreply.github.com>
2020-07-24 13:43:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ def features():
     return render_template('features.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
     app.secret_key = 'some secret key'
